d2eb288dd26a6c63e33c0752db083d7cef26c2b3
Sangwoo Park <sparkly205@gmail.com>
2020-11-03 08:16:25 -0500

    new

--- plot_titration.py
+++ plot_titration.py
@@ -18,16 +18,24 @@ def read_data (fname):
         mean_list.append(mean)
         std_list.append(std)
     return conc_list, mean_list, std_list
-conc_list, mean_list, std_list = read_data("/home/spark159/script/condense-seq/BC1_6_NCP_spermine.csv")
+#conc_list, mean_list, std_list = read_data("/home/spark159/script/condense-seq/BC1_6_NCP_spermine.csv")
+#conc_list, mean_list, std_list = read_data("/home/spark159/script/condense-seq/BC1_6_NCP_HP1a_with_PEG.csv")
+conc_list, mean_list, std_list = read_data("/home/spark159/script/condense-seq/H1_NCP_spermidine.csv")
 
+#del conc_list[0]
+#del mean_list[0]
+#del std_list[0]
 
 fig = plt.figure()
 plt.plot(conc_list, mean_list, 'bo-')
 plt.errorbar(conc_list, mean_list, yerr=std_list, fmt='.')
-plt.xlabel("Spermine concentration (mM)")
+plt.xlabel("Spermidine concentration (mM)")
+#plt.xlabel("HP1$\\alpha$ concentration ($\\mu$M)")
 plt.ylabel("Soluble fraction")
 plt.xscale("log")
-plt.title("H1 NCP spermine(4+)")
+#plt.xscale('log', basex=2)
+#plt.title("BC1~6 NCP mix spermine ($4+$)")
+#plt.title("BC1~6 NCP mix HP1$\\alpha$")
 plt.show()
 plt.close()
 
