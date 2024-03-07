2ffaeba1810c6d10fafd868f349fb09f78a2479e
Sangwoo Park <sangwoopark@sangwoos-air.dfci.harvard.edu>
2024-03-07 09:56:31 -0500

    new

--- prepro_scripts/get_Chalf.py
+++ prepro_scripts/get_Chalf.py
@@ -98,7 +98,7 @@ def get_Chalf (fnames,
                     except:
                         col_ed = len(cols)
 
-                assert len(col_ed - col_st) == len(tnums) # data col len == titration num
+                assert len(tnums) == col_ed - col_st # data col len == titration num
                 s = cols[:col_st]
                 s += ['C-half',
                       'height',
@@ -133,15 +133,18 @@ def get_Chalf (fnames,
             try:
                 p0 = [max(Y), np.median(X), 1]
                 bounds = ([0.0, 0.0, 0.0], [max(Y)+max(Y)*0.1, np.inf, np.inf])
+                
                 popt, pcov = curve_fit(logistic_func,
                                        X,
                                        Y,
                                        p0,
                                        bounds = bounds,
                                        method='dogbox')
+
                 residuals = np.asarray(Y)- logistic_func(X, *popt)
                 ss_res = np.sum(residuals**2)
                 ss_tot = np.sum((np.asarray(Y)-np.mean(Y))**2)
+
                 r_squared = 1 - (ss_res / ss_tot) # fitting quality
                 height, Chalf, rate = popt[:3] # fitting parameters
             except:
@@ -171,7 +174,7 @@ def get_Chalf (fnames,
 
     # summarize output
     print >> sys.stderr, "fitting failure %d/%d" % (fail_count, total_count)
-    print >> sys.stderr, "fitting failure %.2f percentage" % (100*float(fail_count)/total_count)
+    print >> sys.stderr, "fitting failure %.2f %" % (100*float(fail_count)/total_count)
     print >> sys.stderr, "Median R-squared of fitting %.2f" % (np.meidian(rsq_list))
     print >> sys.stderr, "Done"
     
@@ -185,7 +188,7 @@ if __name__ == '__main__':
         else:
             raise argparse.ArgumentTypeError('Boolean value expected.')
 
-    parser = ArgumentParser(description='Compute the condensation point (C-half) by fitting data with a logistic function')
+    parser = ArgumentParser(description='Compute the condensation point (C 1/2) by fitting data with a logistic function')
     parser.add_argument(metavar='-f',
                         dest="fnames",
                         type=str,
@@ -200,6 +203,11 @@ if __name__ == '__main__':
                         type=int,
                         nargs='+',
                         help='titration number of each columns in num data')
+    parser.add_argument('--min_tnum',
+                        dest="min_tnum",
+                        type=int,
+                        default=3,
+                        help='minimum titration data number for fitting')
     parser.add_argument('--min_rsq',
                         dest="min_rsq",
                         type=float,
@@ -221,9 +229,8 @@ if __name__ == '__main__':
     # check titration point of input control is included
     if 0 not in args.tnums:
         print >> sys.stderr, "input control data should be included (titration number is zero)"
-
     # check at least 3 titration points for fitting
-    if len(args.tnums) < 3:
+    if len(args.tnums) < args.min_tnum:
         print >> sys.stderr, "Need more data points for fitting"
     
     # list target chromosomes
